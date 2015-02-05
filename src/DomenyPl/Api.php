40b8a5b8db8020befb5e2f9f3bc006a0a885515b
Adrian 'Nexces' Piotrowicz <adrian.piotrowicz@sprint.pl>
2015-02-05 09:59:40 +0100

    PHP CodeSniffer is now happy :)

--- src/DomenyPl/Api.php
+++ src/DomenyPl/Api.php
@@ -12,34 +12,46 @@ class Api
      * @param string $strLogin
      * @param string $strPassword
      */
-    public function __construct( $strLogin, $strPassword )
+    public function __construct($strLogin, $strPassword)
     {
         $this->strLogin = $strLogin;
         $this->strPassword = $strPassword;
     } // __construct
 
-    public function sendCommand( $strCommandName, array $arrCommandParams = array() )
+    public function sendCommand($strCommandName, array $arrCommandParams = array())
     {
-        $resHandle = curl_init( $this->getApiUrl() );
-        if ( $resHandle === false ) {
-            throw new \Exception( 'An error occurred during CURL call initialization.' );
+        $resHandle = curl_init($this->getApiUrl());
+        if ($resHandle === false) {
+            throw new \Exception('An error occurred during CURL call initialization.');
         }
-        curl_setopt( $resHandle, CURLOPT_HEADER, false );
-        curl_setopt( $resHandle, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt($resHandle, CURLOPT_HEADER, false);
+        curl_setopt($resHandle, CURLOPT_RETURNTRANSFER, 1);
         $arrCommandParams[ 'login' ] = $this->getApiLogin();
         $arrCommandParams[ 'password' ] = $this->getApiPassword();
         $arrCommandParams[ 'command' ] = $strCommandName;
-        $boolResult = curl_setopt( $resHandle, CURLOPT_POSTFIELDS, http_build_query( array( 'params' =>
-            json_encode( $arrCommandParams ) ), '', '&' ) );
-        if ( ! $boolResult ) {
-            throw new \Exception( 'Cant\'t assign "postfields" parameter' );
+        $boolResult = curl_setopt(
+            $resHandle,
+            CURLOPT_POSTFIELDS,
+            http_build_query(
+                array(
+                    'params' => json_encode($arrCommandParams)
+                ),
+                '',
+                '&'
+            )
+        );
+        if (!$boolResult) {
+            throw new \Exception('Cant\'t assign "postfields" parameter');
         }
-        $mixResult = curl_exec( $resHandle );
-        if ( curl_errno( $resHandle ) != 0 ) {
-            throw new \Exception( 'Curl task failed: (Error:'.curl_errno( $resHandle ).') "'.curl_error(
-                    $resHandle ).'" (URL - '.$this->getApiUrl().')' );
+        $mixResult = curl_exec($resHandle);
+        if (curl_errno($resHandle) != 0) {
+            throw new \Exception(
+                'Curl task failed: (Error:'.curl_errno($resHandle).') "'.curl_error(
+                    $resHandle
+                ).'" (URL - '.$this->getApiUrl().')'
+            );
         }
-        return json_decode( $mixResult, true );
+        return json_decode($mixResult, true);
     } // sendCommand
 
     /**
@@ -71,4 +83,4 @@ class Api
     {
         return $this->strPassword;
     } // getApiPassword
-} // class Api
+}
